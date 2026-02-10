81b869f2ba14bed8387eab0ef2bd9d41e3735da0
Dustin <redhotblood1010@gmail.com>
2026-02-10 11:42:21 -0500

    yes

--- M3_ICantseeName/ViewController.swift
+++ M3_ICantseeName/ViewController.swift
@@ -2,7 +2,7 @@
 //  ViewController.swift
 //  M3_ICantseeName
 //
-//  Created by Heather Bishop on 2/10/26.
+//  Created by Dustin Lay on 2/10/26.
 //
 
 import UIKit
@@ -11,9 +11,23 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        print("FirstVC viewDidLoad")
     }
-
-
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("FirstVC viewWillAppear")
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("FirstVC viewDidAppear")
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("FirstVC viewWillDisappear")
+    }
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("FirstVC viewDidDisappear")
+    }
 }
 
--- SecondViewController.swift
+++ SecondViewController.swift
@@ -11,8 +11,23 @@ class SecondViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        print("SecondVC viewDidLoad")
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        print("SecondVC viewWillAppear")
+    }
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        print("SecondVC viewDidAppear")
+    }
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        print("SecondVC viewWillDisappear")
+    }
+    override func viewDidDisappear(_ animated: Bool) {
+        super.viewDidDisappear(animated)
+        print("SecondVC viewDidDisappear")
     }
     
 
